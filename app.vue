cc329422f2cd10b60d8f27375c8ceb8f50ba0cba
Daniel Rivers <me@danielrivers.com>
2022-04-15 20:45:56 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const { result, search } = useSearch('irrelivant')
+const facet = { 
+    name: 'category',
+    query: 'phone'
+   }
+  await search({ facet })
+</script>
